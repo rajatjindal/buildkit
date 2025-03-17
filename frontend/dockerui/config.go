04416590ed9da56c4abcaf717bf12d40ef3ba87e
Rajat Jindal <rajatjindal83@gmail.com>
2025-03-17 13:54:44 +0530

    fixup
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- frontend/dockerui/config.go
+++ frontend/dockerui/config.go
@@ -482,13 +482,13 @@ func (bc *Client) DockerIgnorePatterns(ctx context.Context) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	if true {
-		return nil, fmt.Errorf("from inside BUILD FUNCTION part 8. %#v", bctx.context)
-	}
+	// if true {
+	// 	return nil, fmt.Errorf("from inside BUILD FUNCTION part 8. %#v", bctx.context)
+	// }
 
-	if bctx.context != nil {
-		return nil, nil
-	}
+	// if bctx.context != nil {
+	// 	return nil, nil
+	// }
 
 	return bc.dockerIgnorePatterns(ctx, bctx)
 }
