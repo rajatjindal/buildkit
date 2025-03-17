c9e18701ff96098fbfd12a344a82c7c33c88edaf
Rajat Jindal <rajatjindal83@gmail.com>
2025-03-17 17:09:48 +0530

    fixup
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- frontend/dockerui/config.go
+++ frontend/dockerui/config.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"path"
 	"strconv"
 	"strings"
@@ -484,8 +483,7 @@ func (bc *Client) DockerIgnorePatterns(ctx context.Context) ([]string, error) {
 	}
 
 	if bctx.context != nil {
-		return nil, fmt.Errorf("bctx.context IS NOT EMPTY")
-		// return nil, nil
+		return nil, nil
 	}
 
 	return bc.dockerIgnorePatterns(ctx, bctx)
